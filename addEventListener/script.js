36f2ca924b67ae74a8f76c023660df95b8b6d7a4
PUSPENDU SAHA <103096175+PUSPENDU154@users.noreply.github.com>
2022-06-10 09:15:52 +0530

    all

--- addEventListener/script.js
+++ addEventListener/script.js
@@ -25,5 +25,43 @@ create.classList.add("add")
      document.getElementById("x").style.boxShadow ="0 0 2px 2px gray"
  })
 
+ create.addEventListener("mouseout",()=>{
+    document.querySelector("div.add").style.background="antiquewhite"
+    document.querySelector("div.add").style.color="black"
+    document.getElementById("x").style.background="red"
+ })
+
+
+
+const elementNEW=document.getElementById("new")
+
+for(let i=0;i<10;i++){
+    const create2=document.createElement("div")
+    create2.classList.add("babu","bittu"+""+i)
+    elementNEW.append(create2)
+}
+
+const outer=document.getElementById("outer")
+const inner=document.getElementById("inner")
+
+outer.addEventListener("mouseout",abc)
+function abc(){
+
+    inner.style.background="black"
+    inner.style.color="white"
+}
+outer.addEventListener("click",()=>{
+    outer.removeEventListener("mouseout",abc)
+})
+
+outer.addEventListener("click",()=>{
+    alert("outer div")
+},false)
+// in USECAPTURE "false" is default
+inner.addEventListener("click",()=>{
+    alert("inner div")
+},true)
+
+
 
  
